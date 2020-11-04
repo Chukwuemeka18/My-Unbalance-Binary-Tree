cd0e71d4ef3bd6ec9f3ba80e8e5cd1c2275a34bd
Chukwuemeka Elendu <ChukwuemekaE91021@gmail.com>
2020-11-04 12:14:23 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+ //typedef char* string;
+
+typedef struct node{
+
+  int num;
+  struct node* left;
+  struct node* right;
+
+}
+node;
+
+
+void print(node* pp){
+
+  //printf("%i\n",pp->num);
+
+  if (!pp->left && !pp->right) 
+    { 
+        printf("  %i",pp->num);  
+        //return; 
+    } 
+  
+  if(pp->left !=NULL){
+    print(pp->left);
+  }
+
+  if(pp->right !=NULL){
+    print(pp->right);
+  }
+
+ 
+}
+
+
+
+void insert(int y, node* p){
+
+  if (p->num > y){
+    if (p->left == NULL){
+        node* a = malloc(sizeof(node));
+        p->left = a;
+        p->left->num = y;
+    }
+    else{
+          insert(y,  p->left);
+    }
+  }
+
+  if (p->num < y){
+    if (p->right == NULL){
+        node* a = malloc(sizeof(node));
+        p->right = a;
+        p->right->num = y;
+    }
+    else{
+          insert(y,  p->right);
+    }
+  }
+  
+
+}
+
+
+
+
+int main(void) {
+  
+  printf("Hello World\n");
+
+  node *n = malloc(sizeof(node));
+  
+  printf("In put root number: ");
+  int x = 0;
+  scanf("%i", &x);
+  printf("\n");
+  n->num = x;
+  
+  int y = 1;
+  while (y != 0){
+    
+    printf("In put number: ");
+    scanf("%i", &y);
+    //printf("\n");
+    insert(y, n);
+
+  }
+
+  print(n);
+
+  return 0;
+}
